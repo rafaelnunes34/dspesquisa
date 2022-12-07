3e1970fac75ee3a1cf3b7daed61ece232f294d65
rafaelnunes34 <rafael.nunes34@gmail.com>
2022-12-07 13:20:39 -0300

    Limpando a estrutura do projeto

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            Ola mundo
-        </p>
-      </header>
-    </div>
+    <h1>Olá mundo</h1>
   );
 }
 
