3812e89815d246b6adbd77081f56484564e7f624
rafaelnunes34 <rafael.nunes34@gmail.com>
2022-12-07 11:04:11 -0300

    GET records

--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -1,9 +1,19 @@
 package com.devsuperior.dspesquisa.repositories;
 
+import java.time.Instant;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dspesquisa.entities.Record;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
+	
+	@Query("SELECT obj FROM Record obj WHERE "
+			+ "(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND "
+			+ "(coalesce(:max, null) IS NULL OR obj.moment <= :max)")
+	Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
 
 }
