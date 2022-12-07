a72d4156b42d6b8a20cab6cd4eb79ab7933a83e7
rafaelnunes34 <rafael.nunes34@gmail.com>
2022-12-07 08:55:33 -0300

    Entities, Repositories

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
@@ -0,0 +1,100 @@
+package com.devsuperior.dspesquisa.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.devsuperior.dspesquisa.entities.enums.Platform;
+
+@Entity
+@Table(name = "tb_game")
+public class Game implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Platform platform;
+	
+	@ManyToOne
+	@JoinColumn(name = "genre_id")
+	private Genre genre;
+	
+	@OneToMany(mappedBy = "game")
+	private List<Record> records = new ArrayList<>();
+	
+	public Game() {
+	}
+
+	public Game(Long id, String title, Platform platform, Genre genre) {
+		this.id = id;
+		this.title = title;
+		this.platform = platform;
+		this.genre = genre;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Platform getPlatform() {
+		return platform;
+	}
+
+	public void setPlatform(Platform platform) {
+		this.platform = platform;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+	
+	public List<Record> getRecords() {
+		return records;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(id, other.id);
+	}
+}
---
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/enums/Platform.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dspesquisa.entities.enums;
+
+public enum Platform {
+	PC,
+	PLAYSTATION,
+	XBOX;
+}
